f9e0b99f99679c4ce5186d54560c391612fdc82c
Harshita Yadav <harshitaumesh06@gmail.com>
2026-08-02 22:36:51 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (41.50%) - LeetSync

---
+++ 405-convert-a-number-to-hexadecimal/convert-a-number-to-hexadecimal.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string toHex(int num) {
+        if (num == 0)
+            return "0";
+
+        string hex = "0123456789abcdef";
+        string ans = "";
+
+        while (num != 0 && ans.size() < 8) {
+            ans += hex[num & 15];
+            num >>= 4;
+        }
+
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
